184afa09bdeaf32993ab0bf7161a781589bf24dd
QUENTIN <quentinmartin1996@hotmail.fr>
2023-10-26 15:41:15 +0200

    cration de varable et concatenation

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+// LES VARIABLES
+   $chaine = "COUCOU";
+   
+
+   $nombre = 5;
+   $tab = array(5,"test");
+
+   //AFFICHAGE DES VARIABLES
+   echo $chaine. '<br>';
+   echo $nombre;
+
+   $prenoms = 'quentin';
+   $noms = 'MARTIN';
+
+   echo $prenoms;
+   echo $noms;
+
+   echo '<br> <br>';
+
+   //LES CONCATENATIONS
+   $fullname = $prenoms.' '.$noms;
+   echo $fullname;
+   echo $prenoms.' '.$noms;
+?>
